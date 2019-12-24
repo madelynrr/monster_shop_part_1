1ddbd643681190e08858ee91d0bd9fd811770264
Linda Le <yueling1222@gmail.com>
2019-12-23 17:21:49 -0700

    Delete merchant order model spec test

--- spec/models/merchant_orders_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MerchantOrder, type: :model do
-  describe 'relationships' do
-    it { should belong_to :merchant }
-    it { should belong_to :order }
-  end
-end
