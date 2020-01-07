9a2f191ba834611d1f0fa29daa7e86fd09c07b47
Linda Le <yueling1222@gmail.com>
2020-01-07 14:34:11 -0700

    Move create merchant/items method to merchant items controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,5 +1,21 @@
 class Merchant::ItemsController < Merchant::BaseController
 
+  def create
+    merchant = Merchant.find(current_user.merchant.id)
+    item = merchant.items.create(item_params)
+      if item.image == ""
+        item.image = "https://literalminded.files.wordpress.com/2010/11/image-unavailable1.png"
+      end
+      if item.save
+        flash[:success] = "You have successfully added an item!"
+        redirect_to "/merchant/items"
+      else
+        flash[:error] = item.errors.full_messages.to_sentence
+        render :new
+      end
+  end
+
+
   def destroy
     item = Item.find(params[:id])
     current_user.merchant.items.delete(item)
