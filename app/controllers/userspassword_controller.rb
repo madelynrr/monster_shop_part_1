c694ca2cea9fd558f89f93cf6a13b32de98b090c
Linda Le <yueling1222@gmail.com>
2020-01-07 09:24:25 -0700

    Revise update method for password controller

--- app/controllers/userspassword_controller.rb
+++ app/controllers/userspassword_controller.rb
@@ -1,7 +1,17 @@
 class UsersPasswordController < ApplicationController
 
     def edit
+    end
 
+    def update
+        if params[:password] == params[:password_confirmation]
+            current_user.update(user_params)
+            redirect_to "/profile"
+            flash[:notice] = "Your password has been updated."
+        elsif params[:password] != params[:password_confirmation]
+            redirect_to "/user/password/edit"
+            flash[:error] = "Password and password confirmation do not match."
+        end
     end
 
 end 
