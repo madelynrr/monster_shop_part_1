76b86c40bf30b4b7225fb9b232168561224e0695
Linda Le <yueling1222@gmail.com>
2020-01-07 10:30:56 -0700

    Add before block for test

--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -1,28 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe 'As a merchant' do
-  describe 'When I am on the items page' do
-    it "I can see my items" do
+RSpec.describe 'As a merchant admin/user' do
+  before(:each) do
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+    
+    @merchant_admin = create(:random_user, role: 2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
+  end
+  describe "When I visit my items page" do
 
+    it "can add new items to sell in my store" do 
+
+      visit "/merchant/items"
+      
+      click_on "Add New Item"
+      binding.pry
+
+      expect(current_path).to eq("/merchant/items/new")
+      fill_in :name, with: new_item.name 
+      binding.pry
+      fill_in :price, with: price
+      fill_in :description, with: description
+      fill_in :image, with: image_url
+      fill_in :inventory, with: inventory
+
+      click_button "Create Item"
+
+      new_item = Item.last
+  
     end
-
-#     As a merchant
-# When I visit my items page
-# I see a link or button to deactivate the item next to each item that is active
-# And I click on the "deactivate" button or link for an item
-# I am returned to my items page
-# I see a flash message indicating this item is no longer for sale
-# I see the item is now inactive
-
-    # it "shows a button to deactivate my active items" do
-    #   mike = create(:random_merchant)
-    #   pencil = items.create(:random_item, merchant_id: mike.id)
-    #
-    #   employee = create(:random_user, role: 3)
-    #
-    #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(employee)
-    #
-    #   visit
-    # end
   end
 end
+
