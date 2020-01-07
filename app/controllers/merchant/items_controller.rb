63a09d6f60b04631f3ee1b3a5dcef65cc0bd6272
Linda Le <yueling1222@gmail.com>
2020-01-07 16:44:25 -0700

    Add functionality for previously entered data to appear in form in new merchant/item form when bad/missing info is submitted

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,6 +1,7 @@
 class Merchant::ItemsController < Merchant::BaseController
 
   def new
+    @item = Item.new(item_params)
   end
 
   def show
@@ -9,15 +10,15 @@ class Merchant::ItemsController < Merchant::BaseController
   
   def create
     merchant = Merchant.find(current_user.merchant.id)
-    item = merchant.items.create(item_params)
-      if item.image == ""
-        item.image = "https://literalminded.files.wordpress.com/2010/11/image-unavailable1.png"
+    @item = merchant.items.create(item_params)
+      if @item.image == ""
+        @item.image = "https://literalminded.files.wordpress.com/2010/11/image-unavailable1.png"
       end
-      if item.save
+      if @item.save
         flash[:success] = "You have successfully added an item!"
         redirect_to "/merchant/items"
       else
-        flash[:error] = item.errors.full_messages.to_sentence
+        flash[:error] = @item.errors.full_messages.to_sentence
         render :new
       end
   end
