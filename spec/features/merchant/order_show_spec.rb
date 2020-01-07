49e04ceebfe8938ab8fc33591f8900887e8298cb
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-01-07 15:05:24 -0700

    Add a before each block
    Co-Authored-By: Madelyn Romero <m.romero013@gmail.com>

--- spec/features/merchant/order_show_spec.rb
+++ spec/features/merchant/order_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'As a merchant employee/admin' do
-  it 'from merchant dashboard can click order id to visit order show page' do
+  before :each do
     merchant = create(:random_merchant)
     merchant2 = create(:random_merchant)
     merchant_employee = create(:random_user, role: 3, merchant_id: merchant.id)
@@ -11,21 +11,22 @@ RSpec.describe 'As a merchant employee/admin' do
     item_2 = create(:random_item, merchant_id: merchant.id)
     item_3 = create(:random_item, merchant_id: merchant2.id)
 
-    order = create(:random_order, user_id: user.id)
-    item_1_order = ItemOrder.create!(item: item_1, order: order, price: item_1.price, quantity: 5)
-    item_2_order = ItemOrder.create!(item: item_2, order: order, price: item_2.price, quantity: 3)
-    item_3_order = ItemOrder.create!(item: item_3, order: order, price: item_3.price, quantity: 9)
+    @order = create(:random_order, user_id: user.id)
+    item_1_order = ItemOrder.create!(item: item_1, order: @order, price: item_1.price, quantity: 5)
+    item_2_order = ItemOrder.create!(item: item_2, order: @order, price: item_2.price, quantity: 3)
+    item_3_order = ItemOrder.create!(item: item_3, order: @order, price: item_3.price, quantity: 9)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_employee)
+  end
 
+  it 'from merchant dashboard can click order id to visit order show page' do
     visit '/merchant'
 
-    within "#pending-#{order.id}" do
-      click_link "#{order.id}"
+    within "#pending-#{@order.id}" do
+      click_link "#{@order.id}"
     end
 
-    expect(current_path).to eq("/merchant/orders/#{order.id}")
-
+    expect(current_path).to eq("/merchant/orders/#{@order.id}")
 
   end
 end
