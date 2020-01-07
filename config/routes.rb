79b5fc44f285a15cfbdd251e8706e91ec14f5565
Linda Le <yueling1222@gmail.com>
2020-01-07 14:35:31 -0700

    Revise get items/new to merchant items controller

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
     patch '/orders/:id', to: 'dashboard#update'
     get '/orders/:id', to: 'orders#show'
     get '/items', to: 'dashboard#show'
-    get '/items/new', to: 'dashboard#new'
+    get '/items/new', to: 'items#new'
     post '/items', to: 'items#create'
     delete '/items/:id', to: 'items#destroy'
     patch 'items/:id', to: 'dashboard#update'
