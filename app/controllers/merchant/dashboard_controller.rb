bda6130810849dbc8fab7065a4fa8f354213495c
Linda Le <yueling1222@gmail.com>
2020-01-07 12:12:58 -0700

    Add default value for image in controller

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -9,6 +9,9 @@ class Merchant::DashboardController < Merchant::BaseController
   def create 
     merchant = Merchant.find(current_user.merchant.id)
     item = merchant.items.create(item_params)
+      if item.image == ""
+        item.image = "https://literalminded.files.wordpress.com/2010/11/image-unavailable1.png"
+      end
       if item.save
         flash[:success] = "You have successfully added an item!"
         redirect_to "/merchant/items"
