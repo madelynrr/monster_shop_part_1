baab40e50ca66c4ccec9608b6256cb8135156d52
Madelyn Romero <m.romero013@gmail.com>
2020-01-08 13:47:44 -0700

    Delete unnecessary action

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,10 +3,6 @@ class Admin::DashboardController < Admin::BaseController
     @orders = Order.all
   end
 
-  def show
-    @order = Order.find(params[:id])
-  end
-
   def update
     order = Order.find(params[:id])
     order.ship
