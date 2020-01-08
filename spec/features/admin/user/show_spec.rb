c32692dba3ca02d25207fa6f5d530c12f883618e
Daniel <danmoran.pro@gmail.com>
2020-01-08 15:15:05 -0700

    added test to not see like to edit users profile

--- spec/features/admin/user/show_spec.rb
+++ spec/features/admin/user/show_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe 'As a admin' do
 
     visit "/admin/users/#{user.id}"
 
-    expect(page).to have_content(user.name)
+    expect(page).to have_content("#{user.name}")
     expect(page).to have_content("#{user.address}")
     expect(page).to have_content("#{user.city}")
     expect(page).to have_content("#{user.state}")
     expect(page).to have_content("#{user.zip_code}")
     expect(page).to have_content("#{user.email}")
 
+    expect(page).to_not have_link("Edit Profile")
   end
 end
