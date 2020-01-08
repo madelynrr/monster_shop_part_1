398ce067b1fd5aa9d5cb35bf3a4c35d4bc185be1
Daniel <danmoran.pro@gmail.com>
2020-01-08 14:23:22 -0700

    removed commented out Userstory

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -40,14 +40,3 @@ RSpec.describe 'As an Admin' do
       expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}")
     end
   end
-
-
-# User Story 52, Admin Merchant Index Page
-#
-# As an admin user
-# When I visit the merchant's index page at "/admin/merchants"
-# I see all merchants in the system
-# Next to each merchant's name I see their city and state
-# The merchant's name is a link to their Merchant Dashboard at routes such as "/admin/merchants/5"
-# I see a "disable" button next to any merchants who are not yet disabled
-# I see an "enable" button next to any merchants whose accounts are disabled
