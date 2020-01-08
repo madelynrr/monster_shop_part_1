a3e602286c87ec5feacc4aba599895f2dda51241
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-01-08 10:37:17 -0700

    remove comments

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -27,18 +27,6 @@ class Merchant::ItemsController < Merchant::BaseController
       end
   end
 
-  # def update
-  #   item = Item.find(params[:id])
-  #   item.toggle_active_status
-  #   if item.active?
-  #     flash[:success] = "#{item.name} is Activated"
-  #   else
-  #     flash[:success] = "#{item.name} is deactivated"
-  #   end
-  #   redirect_to '/merchant/items'
-  # end
-
-
   def destroy
     item = Item.find(params[:id])
     current_user.merchant.items.delete(item)
