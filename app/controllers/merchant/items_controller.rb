ba0205c7f8557e2b851142d6e39492ecbcf98cd8
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-09 11:39:13 -0700

    Create helpers for create method

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -15,21 +15,13 @@ class Merchant::ItemsController < Merchant::BaseController
   def create
     merchant = Merchant.find(current_user.merchant.id)
     @item = merchant.items.create(item_params)
-      if @item.image == ""
-        @item.image = "https://literalminded.files.wordpress.com/2010/11/image-unavailable1.png"
-      end
-      if @item.save
-        flash[:success] = "You have successfully added an item!"
-        redirect_to "/merchant/items"
-      else
-        flash[:error] = @item.errors.full_messages.to_sentence
-        render :new
-      end
+    give_default_image_if_needed(@item)
+    item_save_conditional(@item)
   end
 
   def destroy
     item = Item.find(params[:id])
-    current_user.merchant.items.delete(item)
+    # current_user.merchant.items.delete(item)
     item.destroy
     flash[:success] = "You deleted #{item.name}"
     redirect_to "/merchant/items"
@@ -68,6 +60,22 @@ class Merchant::ItemsController < Merchant::BaseController
     else
       flash[:error] = item.errors.full_messages.to_sentence
       render :edit
+    end
+  end
+
+  def give_default_image_if_needed(item)
+    if item.image == ""
+      item.image = "https://literalminded.files.wordpress.com/2010/11/image-unavailable1.png"
+    end
+  end
+
+  def item_save_conditional(item)
+    if item.save
+      flash[:success] = "You have successfully added an item!"
+      redirect_to "/merchant/items"
+    else
+      flash[:error] = item.errors.full_messages.to_sentence
+      render :new
     end
   end
 end
