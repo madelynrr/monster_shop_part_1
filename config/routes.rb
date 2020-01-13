c707115aa5e75a7a2cdc09c1e00deaa14740ec61
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 14:33:54 -0700

    Add route for coupon show page

--- config/routes.rb
+++ config/routes.rb
@@ -68,6 +68,7 @@ Rails.application.routes.draw do
     get '/coupons', to: 'coupons#index'
     get '/coupons/new', to: 'coupons#new'
     post '/coupons', to: 'coupons#create'
+    get '/coupons/:id', to: 'coupons#show'
   end
 
   namespace :admin do
