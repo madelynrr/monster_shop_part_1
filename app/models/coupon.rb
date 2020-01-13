b0a518ae9bb8280476382db3fbd33a084d5347eb
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 14:07:39 -0700

    Add validations so percentage is between 1 and 100

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,6 +2,7 @@ class Coupon < ApplicationRecord
 
   validates_presence_of :name, :code, :percentage
   validates_uniqueness_of :name, :code
+  validates_inclusion_of :percentage, in: 1..100
 
   belongs_to :merchant
 
