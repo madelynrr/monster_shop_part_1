95a37439ba86488f3ddaaa216bf350f1d760913d
Madelyn Romero <m.romero013@gmail.com>
2020-01-12 19:19:39 -0700

    Change to factory bot merchants

--- spec/features/coupons/index_spec.rb
+++ spec/features/coupons/index_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "as a merchant user" do
-  it "can navigate to the coupon index page and see all coupons" do
-    merchant_company = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+  it "can navigate to the coupon index page and see all coupons belonging to it" do
+    merchant_1 = create(:random_merchant)
+    merchant_2 = create(:random_merchant)
 
-    merchant_admin = User.create(name: "Jordan",
+    merchant_1_admin = User.create(name: "Jordan",
                            address: "394 High St",
                            city: "Denver",
                            state: "CO",
@@ -12,9 +13,10 @@ RSpec.describe "as a merchant user" do
                            email: "hotones@hotmail.com",
                            password: "password",
                            password_confirmation: "password",
-                           role: 2)
+                           role: 2,
+                           merchant_id: merchant_1.id)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1_admin)
 
     visit "/merchant/coupons"
 
