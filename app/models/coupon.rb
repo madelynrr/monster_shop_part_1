3a2e43f0a9d13c6a545d3cd5bc6d8f15f868f01d
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 14:15:10 -0700

    Change error message for percentage

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,7 +2,7 @@ class Coupon < ApplicationRecord
 
   validates_presence_of :name, :code, :percentage
   validates_uniqueness_of :name, :code
-  validates_inclusion_of :percentage, in: 1..100
+  validates_inclusion_of :percentage, in: 1..100, message: "needs to be between 1 and 100."
 
   belongs_to :merchant
 
