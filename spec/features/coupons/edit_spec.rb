0aa3028ce47c2d7099e741049661d53df6a10aa0
Madelyn Romero <m.romero013@gmail.com>
2020-01-14 14:24:12 -0700

    Add test to edit coupon

---
+++ spec/features/coupons/edit_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe "as a merchant user" do
+  it "can edit a coupon from the coupon show page" do
+    merchant_1 = create(:random_merchant)
+    merchant_1_admin = User.create(name: "Jordan",
+                           address: "394 High St",
+                           city: "Denver",
+                           state: "CO",
+                           zip_code: "80602",
+                           email: "hotones@hotmail.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: 2,
+                           merchant_id: merchant_1.id)
+
+      coupon_1 = Coupon.create(name: "10% Off",
+                               code: "1234",
+                               percentage: 10,
+                               merchant_id: merchant_1.id)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1_admin)
+
+      visit "/merchant/coupon/#{coupon_1.id}"
+
+      click_button "Edit Coupon"
+
+      expect(current_path).to eq("/merchant/coupon/#{coupon_1.id}/edit")
+
+      name = "20% Off"
+      code = "2345"
+      percentage = 20
+
+      fill_in :name, with: name
+      fill_in :code, with: code
+      fill_in :percentage, with: percentage
+
+      click_button "Update Coupon"
+
+      updated_coupon = Coupon.last
+
+      expect(current_path).to eq("/merchant/coupons/#{coupon_1.id}")
+      expect(page).to have_content(updated_coupon.name)
+      expect(page).to have_content(updated_coupon.code)
+      expect(page).to have_content(updated_coupon.percentage)
+  end
+end
