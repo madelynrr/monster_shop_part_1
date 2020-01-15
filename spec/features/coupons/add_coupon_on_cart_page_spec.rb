cd79967dd1e659018030cd4f7e729769a0e1c575
Madelyn Romero <m.romero013@gmail.com>
2020-01-14 19:46:10 -0700

    Add test to add coupon from cart show page

---
+++ spec/features/coupons/add_coupon_on_cart_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "as a visitor" do
+  it "can add a coupon to order" do
+    merchant = create(:random_merchant)
+    item = create(:random_item, merchant_id: merchant.id)
+    coupon_1 = Coupon.create(name: "10% Off",
+                             code: "1234",
+                             percentage: 10,
+                             merchant_id: merchant.id)
+
+    visit "/items/#{item.id}"
+    click_button "Add To Cart"
+    visit "/cart"
+
+    fill_in :coupon_code, with: coupon_1.code
+
+    click_button "Add Coupon To Order"
+
+    expect(current_path).to eq("/cart")
+    expect(session[:coupon]).to eq(coupon_1.id)
+  end
+end
