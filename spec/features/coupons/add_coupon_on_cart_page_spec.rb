773e7722a659c355db0805f1b7503a977303bdb1
Madelyn Romero <m.romero013@gmail.com>
2020-01-14 21:08:24 -0700

    Fix typo

--- spec/features/coupons/add_coupon_on_cart_page_spec.rb
+++ spec/features/coupons/add_coupon_on_cart_page_spec.rb
@@ -22,6 +22,24 @@ RSpec.describe "as a visitor" do
     visit "/items"
     visit "/cart"
 
-    expect(find_field("Coupon Code").value).to eq(coupon_1.code)
+    expect(find_field("Coupon code").value).to eq(coupon_1.code)
+  end
+
+  it "show error message if coupon code not in system" do
+    merchant = create(:random_merchant)
+    item = create(:random_item, merchant_id: merchant.id)
+
+    visit "/items/#{item.id}"
+    click_button "Add To Cart"
+    visit "/cart"
+
+    coupon_code = 1234
+
+    fill_in :coupon_code, with: coupon_code
+
+    click_button "Add Coupon To Order"
+
+    expect(current_path).to eq("/cart")
+    expect(page).to have_content("#{coupon_code} is not a valid coupon code.")
   end
 end
