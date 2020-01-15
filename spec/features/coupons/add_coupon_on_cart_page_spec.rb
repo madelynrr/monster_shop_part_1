a58a2357f44fcbbf424f387aeb268287f2690e94
Madelyn Romero <m.romero013@gmail.com>
2020-01-14 21:22:27 -0700

    Add to test so user continues shopping and coupon is still remembered

--- spec/features/coupons/add_coupon_on_cart_page_spec.rb
+++ spec/features/coupons/add_coupon_on_cart_page_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe "as a visitor" do
   it "can add a coupon to order" do
     merchant = create(:random_merchant)
-    item = create(:random_item, merchant_id: merchant.id)
+    item_1 = create(:random_item, merchant_id: merchant.id)
+    item_2 = create(:random_item, merchant_id: merchant.id)
     coupon_1 = Coupon.create(name: "10% Off",
                              code: "1234",
                              percentage: 10,
                              merchant_id: merchant.id)
 
-    visit "/items/#{item.id}"
+    visit "/items/#{item_1.id}"
     click_button "Add To Cart"
+
     visit "/cart"
 
     fill_in :coupon_code, with: coupon_1.code
@@ -19,7 +21,9 @@ RSpec.describe "as a visitor" do
 
     expect(current_path).to eq("/cart")
 
-    visit "/items"
+    visit "/items/#{item_2.id}"
+    click_button "Add To Cart"
+
     visit "/cart"
 
     expect(find_field("Coupon code").value).to eq(coupon_1.code)
