e4a47b33c2d3654f6c2a726eb17187abaf3317fd
Madelyn Romero <m.romero013@gmail.com>
2020-01-15 00:18:39 -0700

    Add test for delete coupon from show page

--- spec/features/coupons/delete_spec.rb
+++ spec/features/coupons/delete_spec.rb
@@ -30,4 +30,32 @@ RSpec.describe "as a merchant user" do
       expect(current_path).to eq("/merchant/coupons")
       expect(page).not_to have_css("#coupon-#{coupon_1.id}")
   end
+
+  it "can delete a coupon from the show page" do
+    merchant_1 = create(:random_merchant)
+    merchant_1_admin = User.create(name: "Jordan",
+                           address: "394 High St",
+                           city: "Denver",
+                           state: "CO",
+                           zip_code: "80602",
+                           email: "hotones@hotmail.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: 2,
+                           merchant_id: merchant_1.id)
+
+      coupon_1 = Coupon.create(name: "10% Off",
+                               code: "1234",
+                               percentage: 10,
+                               merchant_id: merchant_1.id)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1_admin)
+
+      visit "/merchant/coupons/#{coupon_1.id}"
+
+      click_button "Delete Coupon"
+
+      expect(current_path).to eq("/merchant/coupons")
+      expect(page).not_to have_css("#coupon-#{coupon_1.id}")
+  end
 end
