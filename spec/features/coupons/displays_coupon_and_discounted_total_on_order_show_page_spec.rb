f562c08b2404e5df95a5159b20a476e5ab248074
Madelyn Romero <m.romero013@gmail.com>
2020-01-15 06:28:28 -0700

    Add test for coupon code and discount total displayed on merchant order show page

--- spec/features/coupons/displays_coupon_and_discounted_total_on_order_show_page_spec.rb
+++ spec/features/coupons/displays_coupon_and_discounted_total_on_order_show_page_spec.rb
@@ -51,4 +51,62 @@ RSpec.describe "as a user" do
     expect(page).to have_content("Coupon Code Used: #{coupon_1.code}")
     expect(page).to have_content("Discount Total: $90.00")
   end
+
+  xit "can display coupon code and discount total on merchant order show page" do
+    merchant_1 = create(:random_merchant)
+    merchant_2 = create(:random_merchant)
+    user = create(:random_user)
+    user_2 = create(:random_user, role: 2)
+    item_1 = create(:random_item, price: 100, merchant_id: merchant_1.id)
+    item_2 = create(:random_item, price: 10, merchant_id: merchant_2.id)
+
+    coupon_1 = Coupon.create(name: "20% Off",
+                             code: "1234",
+                             percentage: 20,
+                             merchant_id: merchant_1.id)
+    visit "/"
+    click_link "Login"
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_button "Login"
+
+    visit "/items/#{item_1.id}"
+    click_button "Add To Cart"
+    visit "/items/#{item_2.id}"
+    click_button "Add To Cart"
+
+    visit "/cart"
+
+    fill_in :coupon_code, with: coupon_1.code
+    click_button "Add Coupon To Order"
+
+    click_link "Checkout"
+
+    name = user.name
+    address = user.address
+    city = user.city
+    state = user.state
+    zip_code = user.zip_code
+
+    fill_in :name, with: name
+    fill_in :address, with: address
+    fill_in :city, with: city
+    fill_in :state, with: state
+    fill_in :zip, with: zip_code
+
+    click_button "Create Order"
+
+    click_link "Log Out"
+    click_link "Login"
+    fill_in :email, with: user_2.email
+    fill_in :password, with: user_2.password
+    click_button "Login"
+
+    last_order = Order.last
+
+    visit "/merchant/orders/#{last_order.id}"
+
+    expect(page).to have_content("Coupon Code Used: #{coupon_1.code}")
+    expect(page).to have_content("Discount Total: $90.00")
+  end
 end
