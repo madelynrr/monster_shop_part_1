2f89bc7aa26f96f895b917eeb990e62bd15f34b5
Madelyn Romero <m.romero013@gmail.com>
2020-01-14 20:48:15 -0700

    Create helper method so coupon code available for display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_admin?, :current_merchant?
+  helper_method :cart, :current_user, :current_admin?, :current_merchant?, :current_coupon
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
 
   def current_admin?
     current_user && current_user.admin?
+  end
+
+  def current_coupon
+    @current_coupon ||= Coupon.find(session[:coupon]) if session[:coupon]
   end
 
 end
