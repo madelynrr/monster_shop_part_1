bd128c0c6d968cdac325adc7c8c2457105992023
Madelyn Romero <m.romero013@gmail.com>
2020-01-24 15:27:01 -0700

    Switch routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,30 +5,12 @@ Rails.application.routes.draw do
   resources :merchants do
     resources :items, only: [:index, :new, :create]
   end
-  # get "/merchants", to: "merchants#index"
-  # get "/merchants/new", to: "merchants#new"
-  # get "/merchants/:id", to: "merchants#show"
-  # post "/merchants", to: "merchants#create"
-  # get "/merchants/:id/edit", to: "merchants#edit"
-  # patch "/merchants/:id", to: "merchants#update"
-  # delete "/merchants/:id", to: "merchants#destroy"
 
-  resources :items, except: [:new, :create]
-  # get "/items", to: "items#index"
-  # get "/items/:id", to: "items#show"
-  # get "/items/:id/edit", to: "items#edit"
-  # patch "/items/:id", to: "items#update"
-  # get "/merchants/:merchant_id/items", to: "items#index"
-  # get "/merchants/:merchant_id/items/new", to: "items#new"
-  # post "/merchants/:merchant_id/items", to: "items#create"
-  # delete "/items/:id", to: "items#destroy"
+  resources :items, except: [:new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id/reviews", to: "reviews#create"
-
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  resources :reviews, only: [:edit, :update, :destroy]
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
@@ -36,11 +18,10 @@ Rails.application.routes.draw do
   delete "/cart/:item_id", to: "cart#remove_item"
   patch '/cart/:item_id', to: 'cart#increment_decrement'
 
-  get "/orders/new", to: "orders#new"
+  resources :orders, only: [:new, :create, :index, :show, :update]
+
   post "/profile/orders", to: "orders#create"
   get '/profile/orders', to: 'orders#index'
-  get "/orders/:id", to: "orders#show"
-  patch "/orders/:id", to: "orders#update"
 
   get '/register', to: 'users#new'
   post '/users', to: 'users#create'
@@ -55,41 +36,31 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   namespace :merchant do
-    get '/', to: 'dashboard#index'
+    root 'dashboard#index'
     get '/profile', to: 'users#index'
     patch '/item_orders/:id', to: 'item_orders#update'
     get '/orders/:id', to: 'orders#show'
-    get '/items', to: 'items#show'
-    get '/items/new', to: 'items#new'
-    post '/items', to: 'items#create'
-    delete '/items/:id', to: 'items#destroy'
-    patch 'items/:id', to: 'items#update'
-    get '/items/:id/edit', to: 'items#edit'
-    get '/coupons', to: 'coupons#index'
-    get '/coupons/new', to: 'coupons#new'
-    post '/coupons', to: 'coupons#create'
-    get '/coupons/:id', to: 'coupons#show'
-    get '/coupons/:id/edit', to: 'coupons#edit'
-    patch '/coupons/:id', to: 'coupons#update'
-    delete '/coupons/:id', to: 'coupons#destroy'
+
+    resources :items, except: [:show]
+
+    resources :coupons
   end
 
   namespace :admin do
-    get '/users', to: 'users#index'
-    get '/users/:id', to: 'users#show'
-    get '/merchants', to: 'merchants#index'
-    get '/', to: 'dashboard#index'
+    root 'dashboard#index'
+    resources :users, only: [:show, :index]
+    resources :merchants, only: [:show, :index, :update]
     get '/profile/:id', to: 'users#show'
-    get '/users', to: 'users#index'
-    get '/users/:id', to: 'users#show'
-    get '/merchants/:id', to: 'merchants#show'
     patch '/orders/:id', to: 'dashboard#update'
-    patch '/merchants/:id', to: 'merchants#update'
   end
 
   get '/user/password/edit', to: 'users_password#edit'
   patch '/user/password/update', to: 'users_password#update'
   patch '/user/id', to: 'users#update'
 
-  post '/coupon_sessions/update', to: 'coupon_sessions#update'
+  resources :coupon_sessions, only: [:create]
+
+
+
+  # post '/coupon_sessions/update', to: 'coupon_sessions#update'
 end
